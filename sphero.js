de66ec4c599f7a7a0ff5ca395544e35d6e738c21
BuildTools <unconfigured@null.spigotmc.org>
2019-04-29 20:45:53 -0400

    jhhjjh

--- sphero.js
+++ sphero.js
@@ -2,6 +2,8 @@ var sphero = require("sphero"),
     orb = sphero("/dev/rfcomm0"); // change port accordingly
  
 orb.connect(function() {
-  // Sphero's connected!
-  // do some cool stuff here!
+  setInterval(function() {
+    var direction = Math.floor(Math.random() * 360);
+    bb8.roll(150, direction);
+  }, 1000);
 });
